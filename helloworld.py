f429b2bdc18bda27dbc9e8932a3b078ab3743166
iajackson <34674321+iajackson@users.noreply.github.com>
2018-05-14 02:11:40 +1000

    Starting update to gui

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Hello World!")
+mainFrame = ttk.Frame(root, width = 640, height = 480)
+mainFrame.grid()
+
 print('Hello world!')
